4a9698cf6ed7606e6cb1545acf6bb28c94eb89f3
Heungsub Lee <sub@subl.ee>
2018-09-13 02:40:50 +0900

    ci

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 set -euo pipefail
 
+dep ensure -v
+rm -rf vendor
+
 while getopts 'x:' opt; do
   echo "$opt"
 done
-
 unset opt
 
-git clone https://github.com/sublee/travis-ci-dep.git
-cd travis-ci-dep
-
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
 dep ensure -v
+rm -rf vendor
